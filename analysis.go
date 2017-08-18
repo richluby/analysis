5c7f2a129999436c593f1e7df18906930b7d951c
Richard Luby <richluby@gmail.com>
2017-08-18 19:04:27 -0400

    added compile-time variables
    versioning and git commit stored as part of binary
    for reproducible building

--- analysis.go
+++ analysis.go
@@ -6,9 +6,24 @@ import (
 	"os"
 )
 
+// Set at compile time
+var (
+	Version string
+	BuildTime string
+)
+
+func printHelp(){
+	fmt.Printf("Data presenter, Version %s, Build %s\n", Version, BuildTime)
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+}
+
 // the entry point of the program
 func main(){
-	fmt.Printf("Usage: %s [options]\n", os.Args[0])
-	readData()
+	printHelp()
+	transactionChannel := make(chan Transaction)
+	if err := readData(transactionChannel); err != nil{
+		fmt.Printf("Error while reading data: %+v", err)
+		os.Exit(1)
+	}
 }
 
