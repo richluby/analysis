63b91dbfe397fae632fccd66d8e49d8e2b293d2f
Richard Luby <richluby@gmail.com>
2017-08-19 12:10:29 -0400

    adds unique values to database
    unique entries get added to databse. status and
    category are not checked when considering uniqueness

--- db.go
+++ db.go
@@ -17,7 +17,7 @@ func initDB(){
 	if err != nil {
 		log.Fatalf("Error while opening database: %+v", err)
 	}
-	createTableStatement := fmt.Sprintf("create table %s (id integer not null primary key, Status text, BusinessName text, Category text, Date text, Amount real);", TABLE_NAME)
+	createTableStatement := fmt.Sprintf("create table %s (id integer not null primary key, Status text, BusinessName text, Category text, Date date, Amount real);", TABLE_NAME)
 	_, err = db.Exec(createTableStatement)
 	if err != nil && !strings.Contains(err.Error(), "table trans already exists"){
 		log.Fatalf("Error while creating table: %+v", err)
@@ -32,7 +32,7 @@ func initDB(){
 //						transactions
 func filterTransactions(receiveChannel chan Transaction, sendChannel chan Transaction){
 	defer close(sendChannel)
-	querystatement, err := database.Prepare(fmt.Sprintf("select * from %s where date = ?", TABLE_NAME))
+	querystatement, err := database.Prepare(fmt.Sprintf("select id, Status, BusinessName, Category, Date, Amount from %s where Date = ?", TABLE_NAME))
 	if err != nil{
 		log.Printf("Error while building query statement: %+v", err)
 		return
@@ -47,9 +47,13 @@ func filterTransactions(receiveChannel chan Transaction, sendChannel chan Transa
 			log.Printf("Dropping transaction: %+v", trans)
 			continue
 		}
+		defer rows.Close()
 		for rows.Next() {
-			rows.Scan(&checkTrans)
-			if checkTrans == trans {
+			err = rows.Scan(&checkTrans.Id, &checkTrans.Status, &checkTrans.BusinessName, &checkTrans.Category, &checkTrans.Date, &checkTrans.Amount)
+			if err != nil{
+				log.Printf("Error while scanning row: %+v", err)
+			}
+			if checkTrans.Compare(trans) == 0 {
 				log.Printf("Duplicate transaction: %+v", checkTrans)
 				transactionFound = true
 				break
@@ -65,9 +69,32 @@ func filterTransactions(receiveChannel chan Transaction, sendChannel chan Transa
 
 // enters data into the database
 // via the provided channel
-func enterData(receiveChannel chan Transaction){
-	for trans := range receiveChannel{
-		fmt.Printf("Transaction: %+v\n", trans)
+func enterData(receiveChannel chan Transaction) error{
+	transactionsAdded := 0
+	transactionHandler, err := database.Begin()
+	if err != nil{
+		log.Printf("Error while creating transaction handler: %+v", err)
+		return err
 	}
+	filterChannel := make(chan Transaction, config.MaxBufferedTransactions)
+	go filterTransactions(receiveChannel, filterChannel)
+	insertStatement, err := transactionHandler.Prepare(fmt.Sprintf("insert into %s(Status, BusinessName, Category, Date, Amount) values(?, ?, ?, ?, ?)", TABLE_NAME))
+	if err != nil{
+		log.Printf("Error while creating insertion statement: %+v", err)
+		return err
+	}
+	defer insertStatement.Close()
+	for trans := range filterChannel {
+		_, err = insertStatement.Exec(trans.Status, trans.BusinessName, trans.Category, trans.Date, trans.Amount)
+		if err != nil{
+			log.Printf("Error while inserting transaction: %+v", err)
+			log.Printf("Dropping transaction: %+v", trans)
+			continue
+		}
+		transactionsAdded += 1
+	}
+	transactionHandler.Commit()
+	fmt.Printf("Transactions added: %d\n", transactionsAdded)
+	return nil
 }
 
